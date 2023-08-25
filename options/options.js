b202785883ddd893a85ab9079570ee883fa0b0fd
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:18:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-dust-30",
+  url: "https://tight-dust-30.botics.co",
+  api: "https://tight-dust-30.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
